f601899023cd486ea0bce32977310c0151418e56
ahyangnb <zonggeu@gmail.com>
2020-06-01 18:19:58 +0800

    commit init

---
+++ ios/Classes/VideoCompressPlugin.m
@@ -0,0 +1,20 @@
+#import "VideoCompressPlugin.h"
+
+@implementation VideoCompressPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"video_compress"
+            binaryMessenger:[registrar messenger]];
+  VideoCompressPlugin* instance = [[VideoCompressPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
