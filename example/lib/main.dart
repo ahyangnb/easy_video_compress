e2bcd671b492b686501a8ce324ec7ed0672beb65
ahyangnb <zonggeu@gmail.com>
2020-06-01 19:15:26 +0800

    commit demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:video_compress/video_compress.dart';
 
 void main() => runApp(MyApp());
@@ -40,12 +43,24 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  selectVideo()async {
+    File video = await ImagePicker.pickVideo(source: ImageSource.gallery);
+    if (video == null) return;
+    VideoCompress.videoCompress(video.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
+          actions: <Widget>[
+            new FlatButton(
+              onPressed: () {},
+              child: new Text('选择'),
+            )
+          ],
         ),
         body: Center(
           child: Text('Running on: $_platformVersion\n'),
