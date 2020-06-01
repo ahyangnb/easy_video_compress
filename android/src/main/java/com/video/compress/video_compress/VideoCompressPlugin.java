54598088b5849fe6686ff93495cfef74250ee4c9
ahyangnb <zonggeu@gmail.com>
2020-06-01 19:09:33 +0800

    commit 视频压缩 Android

--- android/src/main/java/com/video/compress/video_compress/VideoCompressPlugin.java
+++ android/src/main/java/com/video/compress/video_compress/VideoCompressPlugin.java
@@ -1,45 +1,158 @@
 package com.video.compress.video_compress;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.nfc.Tag;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.video.compress.video_compress.plugin.VideoCompress;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** VideoCompressPlugin */
-public class VideoCompressPlugin implements FlutterPlugin, MethodCallHandler {
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "video_compress");
-    channel.setMethodCallHandler(new VideoCompressPlugin());
-  }
+/**
+ * VideoCompressPlugin
+ */
+public class VideoCompressPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "video_compress");
-    channel.setMethodCallHandler(new VideoCompressPlugin());
-  }
+    String TAG = "VideoCompress";
+    static Context ctx;
 
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    private long startTime, endTime;
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "video_compress");
+
+        channel.setMethodCallHandler(new VideoCompressPlugin());
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-  }
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "video_compress");
+        channel.setMethodCallHandler(new VideoCompressPlugin());
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method.equals("videoCompress")) {
+            String path = call.argument("path");
+            handleVideo(path);
+            result.success("已经在处理视频啦");
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    public void handleVideo(String srcPath) {
+        String destPath;
+//        String srcPath = "";
+        destPath = File.separator + "out_VID_" + new SimpleDateFormat("yyyyMMdd_HHmmss", getLocale()).format(new Date()) + ".mp4";
+
+        VideoCompress.compressVideoLow(srcPath, destPath, new VideoCompress.CompressListener() {
+            @Override
+            public void onStart() {
+
+//                tv_indicator.setText("Compressing..." + "\n"
+//                        + "Start at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
+//                pb_compress.setVisibility(View.VISIBLE);
+                startTime = System.currentTimeMillis();
+                Util.writeFile(ctx, "Start at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()) + "\n");
+            }
+
+            @Override
+            public void onSuccess() {
+//                String previous = tv_indicator.getText().toString();
+//                tv_indicator.setText(previous + "\n"
+//                        + "Compress Success!" + "\n"
+//                        + "End at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
+//                pb_compress.setVisibility(View.INVISIBLE);
+                endTime = System.currentTimeMillis();
+                Util.writeFile(ctx, "End at: " + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()) + "\n");
+                Util.writeFile(ctx, "Total: " + ((endTime - startTime) / 1000) + "s" + "\n");
+                Util.writeFile(ctx);
+
+//                startActivity(new Intent(ctx, VideoActivity.class).putExtra("vvVideo", destPath));
+            }
+
+            @Override
+            public void onFail() {
+//                tv_indicator.setText("Compress Failed!");
+//                pb_compress.setVisibility(View.INVISIBLE);
+                endTime = System.currentTimeMillis();
+                Util.writeFile(ctx, "Failed Compress!!!" + new SimpleDateFormat("HH:mm:ss", getLocale()).format(new Date()));
+            }
+
+            @Override
+            public void onProgress(float percent) {
+                Log.d(TAG, "压缩进度：" + percent + "%");
+//                tv_progress.setText(String.valueOf(percent) + "%");
+            }
+        });
+    }
+
+    private Locale getLocale() {
+        Configuration config = ctx.getApplicationContext().getResources().getConfiguration();
+        Locale sysLocale = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            sysLocale = getSystemLocale(config);
+        } else {
+            sysLocale = getSystemLocaleLegacy(config);
+        }
+
+        return sysLocale;
+    }
+
+    @SuppressWarnings("deprecation")
+    public static Locale getSystemLocaleLegacy(Configuration config) {
+        return config.locale;
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    public static Locale getSystemLocale(Configuration config) {
+        return config.getLocales().get(0);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        ctx = binding.getApplicationContext();
+    }
+
+    @Override
+    public void onAttachedToActivity(ActivityPluginBinding binding) {
+        ctx = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+        ctx = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+
+    }
 }
--- lib/video_compress.dart
+++ lib/video_compress.dart
@@ -3,11 +3,17 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class VideoCompress {
-  static const MethodChannel _channel =
-      const MethodChannel('video_compress');
+  static const MethodChannel _channel = const MethodChannel('video_compress');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  /*
+  * 视频压缩
+  * */
+  static videoCompress(path) {
+    _channel.invokeMethod('videoCompress', {"path": path});
   }
 }
