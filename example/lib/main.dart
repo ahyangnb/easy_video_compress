5b5d4a49f09ca219c5876d7463fb38dc6b460caf
ahyangnb <zonggeu@gmail.com>
2020-07-04 11:32:55 +0800

    commit example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,13 @@
+import 'dart:io';
+
+import 'package:easy_video_compress/easy_video_compress.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:easy_video_compress/easy_video_compress.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
@@ -14,12 +15,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  String _chargingStatus = 'Unknown';
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    print('Flutter:::aaaaaa');
+
+    EasyVideoCompress().onMessage.listen((dynamic onData) {
+      print('Flutter:::${onData.toString()}');
+      setState(() {
+        _chargingStatus = onData.toString();
+      });
+    });
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -38,8 +47,22 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      _chargingStatus = platformVersion;
     });
+  }
+
+  selectVideo() async {
+    File video = await ImagePicker.pickVideo(source: ImageSource.gallery);
+    if (video == null) return;
+
+    DateTime d = DateTime.now();
+    String download = await EasyVideoCompress().getPath;
+    String dateStr =
+        'video_${d.year}_${d.month}_${d.day}_${d.hour}_${d.minute}_${d.second}.mp4';
+    print('完整路径：：$download$dateStr');
+    bool result =
+    await EasyVideoCompress().easyVideoCompress(video.path, '$download$dateStr');
+    print('压缩结果::$result');
   }
 
   @override
@@ -48,9 +71,15 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
+          actions: <Widget>[
+            new FlatButton(
+              onPressed: () => selectVideo(),
+              child: new Text('选择'),
+            )
+          ],
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('Running on: $_chargingStatus\n'),
         ),
       ),
     );
